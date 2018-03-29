6ba711b05511488af036267abfda49d62e75b8fd
maciekmacku <maciejszalaj@gmail.com>
2018-03-29 21:59:34 +0200

    added comments to sripts js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// pierwszy komentarz dla polecenia skryptu alert
+alert('test'); 
+
+/*
+komentarz blokowy dla polecenia z zadania 8.2
+var name = prompt('Podaj swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+*/
+
+
+// komentarz dla anulowanego skryptu ponizej
+// console.log('to nie bedzie wyswietlone');
